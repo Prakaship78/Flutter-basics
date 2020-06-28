aec2743bb532f56a26c1f358045a18da6677526a
prakaship78 <prakaship78@gmail.com>
2020-06-28 20:51:05 +0530

    drawerApp: routes * how to use routes * connecting category page

--- drawerApp/lib/MyHomePage.dart
+++ drawerApp/lib/MyHomePage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:drawerApp/MyHomePage.dart';
 import 'Category.dart';
 
 class MyhomePage extends StatefulWidget {
@@ -10,12 +9,29 @@ class MyhomePage extends StatefulWidget {
 class _MyhomePageState extends State<MyhomePage> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: ' Simple Drawer',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Drawer App'),
       ),
-      home: MyhomePage(),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text('Prakash'),
+              accountEmail: Text('Prakaship78@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.orange,
+                child: Text('PS'),
+              ),
+            ),
+            ListTile(
+              title: Text('Category'),
+              trailing: Icon(Icons.card_travel),
+              onTap: () => Navigator.of(context).pushNamed('/a'),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- drawerApp/lib/main.dart
+++ drawerApp/lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'MyHomePage.dart';
+import 'Category.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: ' Simple Drawer',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyhomePage(),
+      routes: <String, WidgetBuilder>{
+        "/a": (BuildContext context) => Category(),
+      },
+    );
   }
 }
